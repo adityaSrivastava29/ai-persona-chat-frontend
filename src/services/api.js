50c62dfaa4044adf912961f5eb199fc0a6ad9b99
adityasrivastava29 <adityasrivastava.niet@gmail.com>
2025-08-15 14:34:47 +0530

    added deployent

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // services/api.js
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5001/api";
+const API_BASE_URL =
+  "https://ai-persona-custom-chat-app-demo1-2cue6xzl9.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ai-persona-chat-frontend/', // Set base to repo name for GitHub Pages
   plugins: [react()],
 })
